f19be187ce00f737c300e4c6c83b2dd52983ef4b
astoddard514 <angela.haugen.stoddard@gmail.com>
2021-12-28 01:25:10 -0600

    First example code about class types

---
+++ 01 - Introduction to OOP/code-along-01.py
@@ -0,0 +1,23 @@
+# We'll be using a store management system to learn about OOP
+
+# Track store items with 4 main variables, related to each other using naming conventions
+item1 = 'Phone'
+item1_price = 100
+item1_quantity = 5
+item1_price_total = item1_price * item1_quantity
+
+# If we were to print the type for each of the variables, like this:
+print(type(item1)) #str
+print(type(item1_price)) #int
+print(type(item1_quantity)) #int
+print(type(item1_price_total)) #int
+
+#Spend some time setting up Python and Powershell
+
+#C:\Users\angel\Desktop\Coding\GitHub\PythonOOP\PythonOOP\01 - Introduction to OOP> py code-along-01.py
+# <class 'str'>
+# <class 'int'>
+# <class 'int'>
+# <class 'int'>
+
+#Tell Python that we want to create a data type of our own
